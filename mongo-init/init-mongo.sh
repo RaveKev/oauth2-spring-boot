fc489f2824563b90d7b828f63874776233472797
RaveKev <github@kevin-busch.com>
2019-09-26 10:37:35 +0200

    Update init-mongo.sh

--- mongo-init/init-mongo.sh
+++ mongo-init/init-mongo.sh
@@ -1,6 +1,6 @@
-mongo -- "$MONGODB_DATABASE" <<EOF
-var user = '$MONGODB_USER';
-var passwd = '$MONGODB_PASS';
+mongo -- "OAUTH_DB" <<EOF
+var user = 'OAUTH_USER';
+var passwd = 'OAUTH_PASS';
 var admin = db.getSiblingDB('admin');
 admin.auth(user, passwd);
 db.createUser({user: user, pwd: passwd, roles: ["readWrite"]});
